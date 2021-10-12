802a8a4274775bab1e86bd7af9aa05af499bee82
Nguyen Thanh Trung <luahantinhthu9@gmail.com>
2021-10-12 11:58:52 +0800

    Add project files.

---
+++ TextLineContain/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace TextLineContain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (!Directory.Exists(@"data"))
+                Directory.CreateDirectory(@"data");
+
+            if (!File.Exists(@"data\fulls.txt"))
+                File.Create(@"data\fulls.txt").Close();
+
+            if (!File.Exists(@"data\lines.txt"))
+                File.Create(@"data\lines.txt").Close();
+
+            string[] fulls = File.ReadAllLines(@"data\fulls.txt");
+            string[] lines = File.ReadAllLines(@"data\lines.txt");
+
+
+            if(fulls.Length > 0 && lines.Length > 0)
+            {
+                foreach (string line in lines)
+                {
+                    bool isContain = false;
+                    foreach (string full in fulls)
+                    {
+                        if(full.Contains(line))
+                        {
+                            using (StreamWriter sw = File.AppendText(@"data\contain.txt"))
+                                sw.WriteLine(full);
+
+                            isContain = true;
+
+                            break;
+                        }
+                    }
+
+                    if(!isContain)
+                        using (StreamWriter sw = File.AppendText(@"data\not_contain.txt"))
+                            sw.WriteLine(line);
+                }
+            }
+
+            Console.WriteLine("Hoan thanh");
+
+            Console.ReadKey();
+        }
+    }
+}
